fdf675a72b33ab67c2df9cfe93232c0bf3a60ce0
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-18 23:10:52 +0300

    Update dec.py

--- praestans/dec.py
+++ praestans/dec.py
@@ -28,12 +28,11 @@ def decimal_to_octal(number):
 @error_handling
 def decimal_to_hexa(number, size):
     hex_str = ""
-    hexadecimal = 16
     hex_chrs = "0123456789abcdef"
     hex_chrs = hex_chrs.upper() if size == "X" else hex_chrs
     number = int(number)
     while (number > 0):
-        left = number % hexadecimal
-        number = number // hexadecimal
+        left = number % 16
+        number = number // 16
         hex_str += hex_chrs[left]
     return (hex_str[::-1])
