061da1f58ef9813b4fb3e17d1a2669218c5a2431
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-19 14:24:24 +0300

    Update dec.py

--- praestans/dec.py
+++ praestans/dec.py
@@ -1,17 +1,30 @@
 from praestans.err import error_handling
 
-# Decimal -> (binary, octal, hexadecimal)
-
-
 @error_handling
 def decimal_to_binray(number):
+    """
+    Converts a decimal (base-10) number to its binary (base-2) representation.
+
+    Parameters:
+        number (int | str): The decimal number to convert.
+
+    Returns:
+        int: An integer representing the binary equivalent of the input number.
+
+    Example:
+        decimal_to_binary(11) -> 1011
+
+    How it works:
+        Divide the number by 2 until it becomes 0, and recod the remainder at each step.
+        Reverse the remainders to obtain the binary number.
+    """
     binary_str = ""
     number = int(number)
     while (number > 0):
         left = number % 2
         number = number // 2
         binary_str += str(left)
-    return (binary_str[::-1])
+    return (int(binary_str[::-1]))
 
 
 @error_handling
