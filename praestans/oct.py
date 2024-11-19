1220e1e673705523e67e3bcdb724e873231d720f
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-19 16:47:37 +0300

    Update oct.py

--- praestans/oct.py
+++ praestans/oct.py
@@ -2,25 +2,53 @@ from praestans.dec import decimal_to_binray
 from praestans.bin import binary_to_hexa
 from praestans.err import error_handling
 
-# octal -> (binary, decimal, hexadecimal)
-
-
 @error_handling
 def octal_to_binary(number):
-    octal_str = str(number)
+    """
+    Converts a octal (base-8) number to its binary (base-2) representation.
+
+    Parameters:
+        number (int | str): The octal number to convert.
+
+    Returns:
+        int: An integer representing the binary equivalent of the input number.
+
+    Example:
+        octal_to_binary(13) -> 1011
+
+    How it works:
+        To convert an octal number to binary, convert each octal digit to its 3-bit binary equivalent. 
+        If the binary equivalent has less than 3 bits, add leading zeros. Finally, combine these 3-bit values to form the binary number.
+    """
     binary_str = ""
+    octal_str = str(number)
     for i in range(len(octal_str)):
         part = str(decimal_to_binray(int(octal_str[i])))
         while len(part) < 3:
             part = "0" + part
         binary_str += part
-    return (binary_str.lstrip("0") or "0")
+    return (int(binary_str.lstrip("0") or "0"))
 
 
 @error_handling
 def octal_to_decimal(number):
-    octal_str = str(number)
+    """
+    Converts a octal (base-8) number to its decimal (base-10) representation.
+
+    Parameters:
+        number (int | str): The octal number to convert.
+
+    Returns:
+        int: An integer representing the decimal equivalent of the input number.
+
+    Example:
+        octal_to_decimal(735) -> 477
+
+    How it works:
+        
+    """
     decimal_sum = 0
+    octal_str = str(number)
     for i in range(len(octal_str)):
         decimal_sum += int(octal_str[len(octal_str) - 1 - i]) * (8 ** i)
     return (decimal_sum)
