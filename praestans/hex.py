513fc20ac3d3dd9ddd0015f53968336dfe074e32
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-19 17:15:19 +0300

    Update hex.py

--- praestans/hex.py
+++ praestans/hex.py
@@ -17,7 +17,8 @@ def hexa_to_decimal(number):
         hexa_to_decimal(ACC342) -> 11322178
 
     How it works:
-       
+        Each digit of the number is multiplied by the power of 16, and these values are summed to obtain the decimal number.
+        Starting from rightmost digit, the value of each digit is multiplied by increasing powers of 16, such as 16^0, 16^1 16^2, and so on.
     """
     result = 0
     hexa_str = str(number)
@@ -37,6 +38,22 @@ def hexa_to_decimal(number):
 
 @error_handling
 def hexa_to_octal(number):
+    """
+    Converts a hexadecimal (base-16) number to its octal (base-8) representation.
+
+    Parameters:
+        number (int | str): The hexadecimal number to convert.
+
+    Returns:
+        int: An integer representing the octal equivalent of the input number.
+
+    Example:
+        hexa_to_octal(ab444) -> 2532104
+
+    How it works:
+        To convert a hexadecimal number to octal, convert each hexadecimal digit to its 4-bit binary equivalent. 
+        Split the binary number into 3-bit groups and convert each group to octal.
+    """
     hexa_str = str(number)
     decimal = hexa_to_decimal(hexa_str)
     binary = decimal_to_binray(decimal)
@@ -48,6 +65,22 @@ def hexa_to_octal(number):
 
 @error_handling
 def hexa_to_binary(number):
+    """
+    Converts a hexadecimal (base-16) number to its binary (base-2) representation.
+
+    Parameters:
+        number (int | str): The hexadecimal number to convert.
+
+    Returns:
+        int: An integer representing the binary equivalent of the input number.
+
+    Example:
+        hexa_to_binary(ee324) -> 11101110001100100100
+
+    How it works:
+        Convert each hexadecimal digit to its 4-bit binary equivalent. 
+        Then, combine these 4-bit binary values to form the binary number.
+    """
     hexa_str = str(number)
     decimal = hexa_to_decimal(hexa_str)
     binary = decimal_to_binray(decimal)
@@ -61,4 +94,4 @@ def hexa_to_binary(number):
         j += 1
     while len(binary) % 4 != 0:
         binary = "0" + binary
-    return (binary)
+    return (int(binary))
