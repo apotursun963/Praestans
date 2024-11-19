6454eef848788fc61000b5e35ea61b7a7e8302bf
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-19 16:56:44 +0300

    Update oct.py

--- praestans/oct.py
+++ praestans/oct.py
@@ -45,17 +45,35 @@ def octal_to_decimal(number):
         octal_to_decimal(735) -> 477
 
     How it works:
-        
+        Each digit of the number is multiplied by the power of 8, and these values are summed to obtain the decimal number.
+        Starting from rightmost digit, the value of each digit is multiplied by increasing powers of 8, such as 8^0, 8^1 8^2, and so on.
     """
-    decimal_sum = 0
+    result = 0
     octal_str = str(number)
     for i in range(len(octal_str)):
-        decimal_sum += int(octal_str[len(octal_str) - 1 - i]) * (8 ** i)
-    return (decimal_sum)
+        result += int(octal_str[len(octal_str) - 1 - i]) * (8 ** i)
+    return (result)
 
 
 @error_handling
 def octal_to_hexa(number):
+    """
+    Converts a octal (base-8) number to its hexadecimal (base-16) representation.
+
+    Parameters:
+        number (int | str): The octal number to convert.
+
+    Returns:
+        str: A string representing the hexadecimal equivalent of the input number.
+
+    Example:
+        octal_to_hexa(3242) -> 6A2
+
+    How it works:
+        To convert an octal number to binary, convert each octal digit to its 3-bit binary equivalent. 
+        Then, combine these 3-bit values to form the binary number. 
+        After that, divide the binary number into 4-bit groups and convert each group to hexadecimal.
+    """
     binar_str = octal_to_binary(number)
     hexa_str = binary_to_hexa(binar_str).lstrip("0")
     return (hexa_str)
